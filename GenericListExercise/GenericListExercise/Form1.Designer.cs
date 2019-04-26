91ff7f5057568bd75bd370c68d838387c1bb1a38
harley <harley.veillette@gmail.com>
2019-04-26 11:36:53 -0300

    Trying to see a grade added correctly

--- GenericListExercise/GenericListExercise/Form1.Designer.cs
+++ GenericListExercise/GenericListExercise/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.TextBox();
+            this.btnFindGrade = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(13, 47);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(55, 17);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "Score:";
+            // 
+            // txtScore
+            // 
+            this.txtScore.Location = new System.Drawing.Point(102, 47);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(109, 22);
+            this.txtScore.TabIndex = 1;
+            // 
+            // btnFindGrade
+            // 
+            this.btnFindGrade.Location = new System.Drawing.Point(102, 88);
+            this.btnFindGrade.Name = "btnFindGrade";
+            this.btnFindGrade.Size = new System.Drawing.Size(109, 32);
+            this.btnFindGrade.TabIndex = 2;
+            this.btnFindGrade.Text = "&Find Grade";
+            this.btnFindGrade.UseVisualStyleBackColor = true;
+            this.btnFindGrade.Click += new System.EventHandler(this.btnFindGrade_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(253, 175);
+            this.Controls.Add(this.btnFindGrade);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtScore;
+        private System.Windows.Forms.Button btnFindGrade;
     }
 }
 
--- GenericListExercise/GenericListExercise/Form1.cs
+++ GenericListExercise/GenericListExercise/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Author: Harley Veillette
+// Date: 2019-04-26
+// Exercise: ArrayLists and Structures 1
+// Notes: This is our first exercise to give back
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +15,39 @@ using System.Windows.Forms;
 
 namespace GenericListExercise
 {
+    public struct Score
+    {
+        public long MaxPoints;
+        public string Grade;
+    }
     public partial class Form1 : Form
     {
+        private Score score;
+        private ArrayList scoreGet = new ArrayList();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnFindGrade_Click(object sender, EventArgs e)
+        {
+            //Code a form that allows Professor Carver to display a grade 
+            //based on the number of points she enters. 
+            try
+            {
+                //Get the string from the textbox and assigning it in the array
+                score.MaxPoints = Convert.ToInt64(txtScore.Text.Trim());
+
+                scoreGet.Add(score);
+                MessageBox.Show($"Grade added to list {score.MaxPoints}");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().ToString());
+
+            }
         }
     }
 }
